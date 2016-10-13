e8bf623f3cd1bac66eb8a17b8cf4a98988286ec0
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2016-10-13 22:02:07 +0200

    Remove CsrfAlwaysRequire since it is hard to implement for GET due to high risk of race condition

--- options.go
+++ options.go
@@ -1,21 +1,19 @@
 package csrf
 
 type Options struct {
-	MaxUsage          int
-	MaxAge            int
-	CookieName        string
-	HeaderName        string
-	Secure            bool
-	CsrfAlwaysRequire bool
+	MaxUsage   int
+	MaxAge     int
+	CookieName string
+	HeaderName string
+	Secure     bool
 }
 
 func DefaultOptions() *Options {
 	return &Options{
-		MaxUsage:          100,
-		MaxAge:            60 * 60,
-		CookieName:        "csrf_token",
-		HeaderName:        "X-CSRF-Token",
-		Secure:            true,
-		CsrfAlwaysRequire: false,
+		MaxUsage:   100,
+		MaxAge:     60 * 60,
+		CookieName: "csrf_token",
+		HeaderName: "X-CSRF-Token",
+		Secure:     true,
 	}
 }
