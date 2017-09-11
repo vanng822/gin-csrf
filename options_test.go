235c044fe534cf214b2147c855c915ad6ea86f34
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2017-09-11 19:41:14 +0200

    #12 test default options

---
+++ options_test.go
@@ -0,0 +1,25 @@
+package csrf
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefaultOptions(t *testing.T) {
+	actual := DefaultOptions()
+	expected := &Options{
+		MaxUsage:         100,
+		MaxAge:           60 * 60,
+		CookieName:       "csrf_token",
+		HeaderName:       "X-CSRF-Token",
+		Secure:           true,
+		UsageCounterName: "csrf_token_usage",
+		SessionName:      "csrf_token_session",
+		IssuedName:       "csrf_token_issued",
+		ByteLenth:        32,
+		Path:             "/",
+		SafeMethods:      []string{"GET", "HEAD", "OPTIONS"},
+	}
+	assert.Equal(t, expected, actual)
+}
