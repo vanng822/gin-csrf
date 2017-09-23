dd31f90bff7189622cd349a309adcf7f1a2b68c0
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2017-09-23 11:43:25 +0200

    #13 patch https scheme and Referer

--- handler_test.go
+++ handler_test.go
@@ -182,3 +182,84 @@ func TestIssuedTimePassed(t *testing.T) {
 	resp, _ = client.Do(req)
 	assert.Equal(t, 400, resp.StatusCode)
 }
+
+func TestHttpsNoReferer(t *testing.T) {
+	router := gin.Default()
+	options := DefaultOptions()
+	options.MaxUsage = 10
+	options.MaxAge = 15 * 60
+	store, _ := sessions.NewRedisStore(10, "tcp", "localhost:6379", "", []byte("something"))
+	router.Use(sessions.Sessions("session", store))
+	router.Use(func(c *gin.Context) {
+		c.Request.URL.Scheme = "https"
+		c.Request.Header.Del("Referer")
+	})
+	router.Use(Csrf(options))
+	ts := httptest.NewTLSServer(router)
+	defer ts.Close()
+
+	client := ts.Client()
+	resp, _ := client.Post(ts.URL, "application/json", strings.NewReader(""))
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+}
+
+func TestHttpsSameReferer(t *testing.T) {
+	router := gin.Default()
+	options := DefaultOptions()
+	options.MaxUsage = 10
+	options.MaxAge = 15 * 60
+	store, _ := sessions.NewRedisStore(10, "tcp", "localhost:6379", "", []byte("something"))
+	router.Use(sessions.Sessions("session", store))
+	router.Use(func(c *gin.Context) {
+		c.Request.URL.Scheme = "https"
+		c.Request.Header.Set("Referer", c.Request.URL.String())
+	})
+	router.Use(Csrf(options))
+	ts := httptest.NewTLSServer(router)
+	defer ts.Close()
+
+	client := ts.Client()
+	resp, _ := client.Post(ts.URL, "application/json", strings.NewReader(""))
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+}
+
+func TestHttpsDifferentReferer(t *testing.T) {
+	router := gin.Default()
+	options := DefaultOptions()
+	options.MaxUsage = 10
+	options.MaxAge = 15 * 60
+	store, _ := sessions.NewRedisStore(10, "tcp", "localhost:6379", "", []byte("something"))
+	router.Use(sessions.Sessions("session", store))
+	router.Use(func(c *gin.Context) {
+		c.Request.URL.Scheme = "https"
+		c.Request.Header.Set("Referer", "http://some.domain.tld")
+	})
+	router.Use(Csrf(options))
+	ts := httptest.NewTLSServer(router)
+	defer ts.Close()
+
+	client := ts.Client()
+	resp, _ := client.Post(ts.URL, "application/json", strings.NewReader(""))
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+}
+
+// func TestHttpsBrokenReferer(t *testing.T) {
+// 	router := gin.Default()
+// 	options := DefaultOptions()
+// 	options.MaxUsage = 10
+// 	options.MaxAge = 15 * 60
+// 	store, _ := sessions.NewRedisStore(10, "tcp", "localhost:6379", "", []byte("something"))
+// 	router.Use(sessions.Sessions("session", store))
+// 	router.Use(func(c *gin.Context) {
+// 		c.Request.URL.Scheme = "https"
+// 		// url.Parse always ok
+// 		c.Request.Header.Set("Referer", "?????")
+// 	})
+// 	router.Use(Csrf(options))
+// 	ts := httptest.NewTLSServer(router)
+// 	defer ts.Close()
+//
+// 	client := ts.Client()
+// 	resp, _ := client.Post(ts.URL, "application/json", strings.NewReader(""))
+// 	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+// }
