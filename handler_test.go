aa2c5488f052099a02f8ced0890e570509ebc4e3
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2017-09-23 08:41:33 +0200

    #13 setup tests for handler

--- handler_test.go
+++ handler_test.go
@@ -1,8 +1,13 @@
 package csrf
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,4 +25,24 @@ func TestIsTokenValidTrue(t *testing.T) {
 
 func TestIsTokenValidFalse(t *testing.T) {
 	assert.False(t, isTokenValid("abc", "bcf"))
+}
+
+func TestFlow(t *testing.T) {
+	router := gin.Default()
+	options := DefaultOptions()
+	options.MaxUsage = 10
+	options.MaxAge = 15 * 60
+	store, _ := sessions.NewRedisStore(10, "tcp", "localhost:6379", "", []byte("asdsd"))
+	router.Use(sessions.Sessions("session", store))
+	router.Use(Csrf(options))
+	router.GET("/", func(c *gin.Context) {
+	})
+	router.POST("/", func(c *gin.Context) {
+	})
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+	resp, _ := http.Get(ts.URL)
+	assert.Equal(t, 200, resp.StatusCode)
+	resp, _ = http.Post(ts.URL, "application/json", strings.NewReader(""))
+	assert.Equal(t, 400, resp.StatusCode)
 }
